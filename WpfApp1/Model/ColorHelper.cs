1e1f79039219c7b14dd1e295d636ed0a46efc169
unknown <LocalHost@PC2021-15.ad.pslib.cz>
2022-03-31 09:11:16 +0200

    first commit

---
+++ WpfApp1/Model/ColorHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp1.Model
+{
+    internal class ColorHelper
+    {
+        public int[,] Pixels { get; set; }
+
+        public void FilterColor()
+        {
+            /*int a = solidColorBrush.Color.A;
+            int r = solidColorBrush.Color.R;
+            int g = solidColorBrush.Color.G;
+            int b = solidColorBrush.Color.B;*/
+
+
+            //int color = (a << 24) + (r << 16) + (g << 8) + b;
+            for (int x = 0; x < Pixels.GetLength(0); ++x)
+            {
+                for (int y = 0; y < Pixels.GetLength(1); ++y)
+                {
+
+                    int a = (int)(Pixels[x, y] & 0xFF000000);
+                    int r = (Pixels[x, y] >> 16) & 0xFF;
+                    int g = (Pixels[x, y] >> 8) & 0xFF;
+                    int b = (Pixels[x, y]) & 0xFF;
+                    int prumer = (r + g + b) / 3;
+                    Pixels[x, y] = a + (prumer << 16) + (prumer << 8) + prumer;
+                }
+            }
+        }
+
+        public void BlueFilter()
+        {
+
+            for (int x = 0; x < Pixels.GetLength(0); ++x)
+            {
+                for (int y = 0; y < Pixels.GetLength(1); ++y)
+                {
+
+                }
+            }
+        }
+
+        public void Flip()
+        {
+            /*int[,] testArray = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] testArray2 = new int[testArray.GetLength(0), testArray.GetLength(1)];
+            for (int x = 0; x < testArray.GetLength(0); ++x)
+            {
+                for (int y = 0; y < testArray.GetLength(1); ++y)
+                {
+                    testArray2[x, y] = testArray[x, testArray.GetLength(1) - (y + 1)];
+                }
+            }*/
+
+
+
+            int[,] refArray = new int[Pixels.GetLength(0), Pixels.GetLength(1)];
+            for (int x = 0; x < Pixels.GetLength(0); ++x)
+            {
+                for (int y = 0; y < Pixels.GetLength(1); ++y)
+                {
+                    refArray[x, y] = Pixels[x, Pixels.GetLength(1) - (y + 1)];
+                }
+            }
+            Pixels = refArray;
+        }
+        public void Reduction()
+        {
+            for (int x = 0; x < Pixels.GetLength(0); ++x)
+            {
+                for (int y = 0; y < Pixels.GetLength(1); ++y)
+                {
+                    int color = Pixels[x, y];
+                    Pixels[x, y] = (int)((int)color & 0xFFF0F0C0);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/BaseViewModel.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private ColorHelper _colorHelper;
+        private BitmapImage _bitmapImage;
+        public BitmapImage BitmapImage
+        {
+            get
+            {
+                return _bitmapImage;
+            }
+            set
+            {
+                _bitmapImage = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ICommand _filePickedCommand;
+        public ICommand FilePickedCommand
+        {
+            get
+            {
+                return (_filePickedCommand = new CommandHandler(
+                    () =>
+                    {
+                        OpenFileDialog openFileDialog = new OpenFileDialog();
+                        openFileDialog.Filter = " (*.png;*.jpeg)|*.png;*.jpeg";
+
+                        if (openFileDialog.ShowDialog() == true)
+                        {
+                            BitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
+                            
+                        }
+
+                    },
+                    () =>
+                    {
+                        return true;
+                    }
+                    ));
+            }
+        }
+        public ICommand FileUnloadCommand
+        {
+            get
+            {
+                return (_filePickedCommand = new CommandHandler(
+                    () =>
+                    {
+                        BitmapImage = null;
+
+                    },
+                    () =>
+                    {
+                        return true;
+                    }
+                    ));
+            }
+        }
+
+        private ICommand _blackFadeCommand;
+        public ICommand BlackFadeCommand
+        {
+            get
+            {
+                return (_blackFadeCommand = new CommandHandler(
+                    () =>
+                    {
+                        _colorHelper.Pixels = Array2DBMIConverter.BitmapImageToArray2D(BitmapImage);
+
+                        _colorHelper.FilterColor();
+
+                        BitmapImage = Array2DBMIConverter.ConvertWriteableBitmapToBitmapImage(Array2DBMIConverter.Array2DToWriteableBitmap(_colorHelper.Pixels, BitmapImage));
+
+
+
+                    },
+                    () =>
+                    {
+                        return BitmapImage == null? false: true;
+                    }
+                    ));
+            }
+        }
+        private ICommand _ReductionCommand;
+        public ICommand ReductionCommand
+        {
+            get
+            {
+                return (_ReductionCommand = new CommandHandler(
+                    () =>
+                    {
+                        _colorHelper.Pixels = Array2DBMIConverter.BitmapImageToArray2D(BitmapImage);
+
+                        _colorHelper.Reduction();
+
+                        BitmapImage = Array2DBMIConverter.ConvertWriteableBitmapToBitmapImage(Array2DBMIConverter.Array2DToWriteableBitmap(_colorHelper.Pixels, BitmapImage));
+
+
+
+                    },
+                    () =>
+                    {
+                        return BitmapImage == null ? false : true;
+                    }
+                    ));
+            }
+        }
+
+        private ICommand _flipCommand;
+        public ICommand FlipCommand
+        {
+            get
+            {
+                return (_flipCommand = new CommandHandler(
+                    () =>
+                    {
+                        _colorHelper.Pixels = Array2DBMIConverter.BitmapImageToArray2D(BitmapImage);
+
+                        _colorHelper.Flip();
+
+                        BitmapImage = Array2DBMIConverter.ConvertWriteableBitmapToBitmapImage(Array2DBMIConverter.Array2DToWriteableBitmap(_colorHelper.Pixels, BitmapImage));
+
+
+
+                    },
+                    () =>
+                    {
+                        return BitmapImage == null ? false : true;
+                    }
+                    ));
+            }
+        }
+
+        public BaseViewModel()
+        {
+            _colorHelper = new ColorHelper();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
